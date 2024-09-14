d2ba67f0be0169c85b7fdaee7163e36103579e2a
Marcos Vieira <marcosvieira482@gmail.com>
2024-09-14 19:59:11 -0300

    add dados.js

---
+++ dados.js
@@ -0,0 +1,101 @@
+const vereadores = [
+    {
+        nome: "Ana Silva",
+        numero: "97001",
+        partido: "ABC",
+        foto: ""
+    },
+    {
+        nome: "João Santos",
+        numero: "97002",
+        partido: "ABC",
+        foto: ""
+    },
+    {
+        nome: "Maria Oliveira",
+        numero: "97003",
+        partido: "ABC",
+        foto: ""
+    },
+    {
+        nome: "Gabriel Almeida",
+        numero: "98001",
+        partido: "DEF",
+        foto: ""
+    },
+    {
+        nome: "Sofia Rodrigues",
+        numero: "98002",
+        partido: "DEF",
+        foto: ""
+    },
+    {
+        nome: "Lucas Costa",
+        numero: "98003",
+        partido: "DEF",
+        foto: ""
+    },
+    {
+        nome: "Beatriz Pereira",
+        numero: "99001",
+        partido: "XYZ",
+        foto: ""
+
+    },
+    {
+        nome: "Rafael Ferreira",
+        numero: "99002",
+        partido: "XYZ",
+        foto: ""
+    },
+    {
+        nome: "Manuela Souza",
+        numero: "99003",
+        partido: "XYZ",
+        foto: ""
+    },
+]
+
+const prefeitos = [
+    {
+        nome: "Laura Marques",
+        vice: "Arthur Gomes",
+        numero: "97",
+        partido: "ABC",
+        foto1: "",
+        foto2: "",
+    },
+    {
+        nome: "Davi Moreno",
+        vice: "Alice Cardosos",
+        numero: "98",
+        partido: "DEF",
+        foto1: "",
+        foto2: "",
+    },
+    {
+        nome: "Bruno Barros",
+        vice: "Isabela Santos",
+        numero: "99",
+        partido: "XYZ",
+        foto1: "",
+        foto2: "",
+    },
+]
+
+const partidos = [
+    {
+        nome: "ABC",
+        legenda: "97",
+    },
+
+    {
+        nome: "DEF",
+        legenda: "98",
+    },
+
+    {
+        nome: "XYZ",
+        legenda: "99",
+    },
+]
