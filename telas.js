c366effbb183301d68bef6a1d31f09e60648f6c0
Marcos Vieira <marcosvieira482@gmail.com>
2024-09-13 21:13:54 -0300

    separate screens in their own file

---
+++ telas.js
@@ -0,0 +1,67 @@
+const telaVereador = `
+            <div class="tela-voto">
+                <div class="infos">
+                    <div>SEU VOTO PARA</div>
+                    <div class="titulo">Vereador</div>
+                    <div class="numero">
+                        <div id="tx-numero">Número:</div>
+                        <div class="num"><span id="n1"></span></div>
+                        <div class="num"><span id="n2"></span></div>
+                        <div class="num"><span id="n3"></span></div>
+                        <div class="num"><span id="n4"></span></div>
+                        <div class="num"><span id="n5"></span></div>
+                    </div>
+                    <div id="nome">Nome:</div>
+                    <div id="partido">Partido:</div>
+                    <div id="atencao"></div>
+                </div>
+                <div class="fotos">
+                    <div id="foto1"></div>
+                </div>
+            </div>
+            <div class="tela-info">
+                Aperte a tecla:
+                <span>CONFIRMA para CONFIRMAR este voto<br>CORRIGE para REINICIAR este voto</span>
+            </div>
+`;
+
+const telaPrefeito = `
+            <div class="tela-voto">
+                <div class="infos">
+                    <div>SEU VOTO PARA</div>
+                    <div class="titulo">Prefeito</div>
+                    <div class="numero">
+                        <div id="tx-numero">Número:</div>
+                        <div class="num"><span id="n1"></span></div>
+                        <div class="num"><span id="n2"></span></div>
+                    </div>
+                    <div id="nome">Nome:</div>
+                    <div id="partido">Partido:</div>
+                    <div id="atencao"></div>
+                </div>
+                <div class="fotos">
+                    <div id="foto1"></div>
+                </div>
+            </div>
+            <div class="tela-info">
+                Aperte a tecla:
+                <span>CONFIRMA para CONFIRMAR este voto<br>CORRIGE para REINICIAR este voto</span>
+            </div>
+`;
+
+const telaGravando = `
+            <div class="tela-gravando">
+                <div class="tx-gravando">GRAVANDO</div>
+                <div class="barra"></div>
+            </div>
+`;
+
+const telaFim = `
+            <div class="tela-fim">
+                <div class="fim">FIM</div>
+                <div class="votou">VOTOU</div>
+            </div>
+`;
+
+
+
