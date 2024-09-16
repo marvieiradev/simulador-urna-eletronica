e29461983f3ca4136293e90e05c15133168b99e1
Marcos Vieira <marcosvieira482@gmail.com>
2024-09-16 11:45:06 -0300

    update dados

--- dados.js
+++ dados.js
@@ -3,56 +3,56 @@ const vereadores = [
         nome: "Ana Silva",
         numero: "97001",
         partido: "ABC",
-        foto: "img/img01"
+        foto: "img/img01.jpg"
     },
     {
         nome: "João Santos",
         numero: "97002",
         partido: "ABC",
-        foto: ""
+        foto: "img/img02.jpg"
     },
     {
         nome: "Maria Oliveira",
         numero: "97003",
         partido: "ABC",
-        foto: ""
+        foto: "img/img03.jpg"
     },
     {
         nome: "Gabriel Almeida",
         numero: "98001",
         partido: "DEF",
-        foto: ""
+        foto: "img/img04.jpg"
     },
     {
         nome: "Sofia Rodrigues",
         numero: "98002",
         partido: "DEF",
-        foto: ""
+        foto: "img/img05.jpg"
     },
     {
         nome: "Lucas Costa",
         numero: "98003",
         partido: "DEF",
-        foto: ""
+        foto: "img/img06.jpg"
     },
     {
         nome: "Beatriz Pereira",
         numero: "99001",
         partido: "XYZ",
-        foto: ""
+        foto: "img/img07.jpg"
 
     },
     {
         nome: "Rafael Ferreira",
         numero: "99002",
         partido: "XYZ",
-        foto: ""
+        foto: "img/img08.jpg"
     },
     {
         nome: "Manuela Souza",
         numero: "99003",
         partido: "XYZ",
-        foto: ""
+        foto: "img/img09.jpg"
     },
 ]
 
@@ -62,24 +62,24 @@ const prefeitos = [
         vice: "Arthur Gomes",
         numero: "97",
         partido: "ABC",
-        foto1: "",
-        foto2: "",
+        foto1: "img/img10.jpg",
+        foto2: "img/img11.jpg",
     },
     {
         nome: "Davi Moreno",
         vice: "Alice Cardosos",
         numero: "98",
         partido: "DEF",
-        foto1: "",
-        foto2: "",
+        foto1: "img/img12.jpg",
+        foto2: "img/img13.jpg",
     },
     {
         nome: "Bruno Barros",
         vice: "Isabela Santos",
         numero: "99",
         partido: "XYZ",
-        foto1: "",
-        foto2: "",
+        foto1: "img/img14.jpg",
+        foto2: "img/img15.jpg",
     },
 ]
 
